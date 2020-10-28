4562e01edcadf1d03d409ee3d73882ff2b7cc508
Michael Partridge <mcp292@nau.edu>
2020-10-27 19:48:43 -0700

    Fix hw6 steps 5 and 6

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -1,45 +1,45 @@
+# Step 1
 import pandas as pd
 import matplotlib
 
+# Step 2
 dataset = pd.read_csv("chipotle.tsv", sep='\t')
 # OR
-# dataset = pd.read_table("chipotle.tsv")
+dataset = pd.read_table("chipotle.tsv")
 
+# Step 3
 chipo = dataset
 
-print(chipo.head(10)) # TODO: fix print
-# print(chipo.columns.tolist())
+# Step 4
+print(chipo.head(10)) 
 
-### Step 5. Create a bar plot of the top 5 items bought (total quantity per item)
-# TODO: limit to 5 cleanly
-chipo_top5 = chipo.sort_values("quantity", ascending=False)
-chipo_top5 = chipo_top5.head(5) # only take top 5 
+# Step 5
+chipo_subset = chipo[["item_name", "quantity"]]                        # extract
+chipo_subset = chipo_subset.groupby("item_name", as_index=False).sum() # groupby
+chipo_subset = chipo_subset.sort_values("quantity", ascending=False)   # sort
+
+chipo_top5 = chipo_subset.head(5) # only take top 5
 
 chipo_plot = chipo_top5.plot.bar("item_name", "quantity")
-chipo_plot.set_ylabel("Quantity")
+chipo_plot.set_xlabel("Item name")
+chipo_plot.set_ylabel("Total sold")
 chipo_plot.set_title("Top 5 Items Bought at Chipotle")
-#matplotlib.pyplot.show()
+matplotlib.pyplot.show()
 
-### Step 6. Create a scatterplot with the number of items orderered per order price
-#### Hint: Price should be in the X-axis and Items ordered in the Y-axis
-#(You will have to groupby and use the result
-#print(chipo.groupby(["item_price", "quantity"]).sum())
-
-#selected_cols = ["item_price", "quantity"]
-#chipo_plot = chipo[selected_cols].groupby(selected_cols, as_index=False).sum()
-
-
-chipo_subset = chipo[["item_price", "quantity"]]
-chipo_subset = chipo_subset.groupby("item_price", as_index=False).sum()
-
-#chipo_plot = chipo[selected_cols].groupby("item_price", as_index=False).sum()
-#chipo_plot = chipo.groupby(["item_price"]).sum()
-print("\n\n\nhere")
-print(chipo_subset.columns.tolist())
-print(chipo_subset.head(5))
+# Step 6
+chipo_subset = chipo[["order_id", "quantity", "item_price"]]          # extract
+chipo_subset = chipo_subset.groupby("order_id", as_index=False).sum() # groupby
 
 chipo_plot = chipo_subset.plot.scatter("item_price", "quantity")
-chipo_plot.set_xlabel("Item Price ($)")
+chipo_plot.set_xlabel("Order Price ($)")
 chipo_plot.set_ylabel("Items Purchased")
-chipo_plot.set_title("Items Purchased vs Item Price")
+chipo_plot.set_title("Items Purchased vs Order Price")
 matplotlib.pyplot.show()
+
+
+# panda links:
+# https://stackoverflow.com/questions/49188960/how-to-show-all-of-columns-name-on-pandas-dataframe/49189503#49189503v
+# https://stackoverflow.com/questions/10373660/converting-a-pandas-groupby-output-from-series-to-dataframe#comment114163633
+# _10374456
+# https://stackoverflow.com/questions/38985053/pandas-groupby-and-sum-only-one-column
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.groupby.html
